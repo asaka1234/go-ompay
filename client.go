7075760fb7d08dd7fef62e5d44624f4b2261eba5
cyrildou <cyrildou@live.cn>
2025-05-31 10:34:02 +0800

    up

--- client.go
+++ client.go
@@ -22,10 +22,6 @@ func NewClient(logger utils.Logger, params OMPayInitParams) *Client {
 }
 
 // 方便依据currency来切换 (MYR, IDR/ / SGD/ THB/ VND)
-func (cli *Client) SetMerchantInfo(merchantId string, depositApiKey string, depositSecretKey string, withdrawAgentCode, withdrawSecretKey string) {
-	cli.Params.MerchantId = merchantId
-	cli.Params.DepositApiKey = depositApiKey
-	cli.Params.DepositSecretKey = depositSecretKey
-	cli.Params.WithdrawAgentCode = withdrawAgentCode
-	cli.Params.WithdrawSecretKey = withdrawSecretKey
+func (cli *Client) SetMerchantInfo(merchant MerchantInfo) {
+	cli.Params.MerchantInfo = merchant
 }
